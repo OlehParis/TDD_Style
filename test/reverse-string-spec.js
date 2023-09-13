505d2cae48dd1ecb11fab6c0e0677f59cf13adff
Oleh <olehparis@Olehs-Air.attlocal.net>
2023-09-13 19:41:30 -0400

    string_reverse

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,42 @@
-// Your code here
+const reverseString = require("../problems/reverse-string.js")
+const {expect} = require("chai")
+describe("reverseString", function(){
+    it("should reverse string", function(){
+        const str = "fun"
+        expect(reverseString(str)).to.be.eql("nuf")
+    })
+})
+
+
+// describe("Computer", function () {
+//     it("should have brand, OS and type properties", function () {
+//       const myComp = new Computer("LG", "Windows", "PC");
+//       expect(myComp).to.have.property("brand");
+//       expect(myComp).to.have.property("OS");
+//       expect(myComp).to.have.property("type");
+//     });
+  
+    
+//       describe("welcome static method", function () {
+//         it("should log the powerOn messages for each computer", function () {
+//           const comp1 = new Computer("Dell", "Windows", "PC");
+//           const comp2 = new Computer("Apple", "MacOS", "Apple");
+  
+//           const consoleLogStub = [];
+//           const originalConsoleLog = console.log;
+//           console.log = (message) => {
+//             consoleLogStub.push(message);
+//           };
+  
+//           Computer.welcome(comp1, comp2);
+  
+//           expect(consoleLogStub).to.deep.equal([
+//             "Dell PC powers on with Windows",
+//             "Apple Apple powers on with MacOS",
+//           ]);
+  
+//           console.log = originalConsoleLog;
+//         });
+//       });
+//     });
+  
