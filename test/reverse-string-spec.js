12cde54b5791525f952da6d4816db704ce5997e6
Tameka Roberson <tamekaroberson1995@gmail.com>
2023-09-13 19:43:07 -0400

    ReverseString done

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -6,37 +6,3 @@ describe("reverseString", function(){
         expect(reverseString(str)).to.be.eql("nuf")
     })
 })
-
-
-// describe("Computer", function () {
-//     it("should have brand, OS and type properties", function () {
-//       const myComp = new Computer("LG", "Windows", "PC");
-//       expect(myComp).to.have.property("brand");
-//       expect(myComp).to.have.property("OS");
-//       expect(myComp).to.have.property("type");
-//     });
-  
-    
-//       describe("welcome static method", function () {
-//         it("should log the powerOn messages for each computer", function () {
-//           const comp1 = new Computer("Dell", "Windows", "PC");
-//           const comp2 = new Computer("Apple", "MacOS", "Apple");
-  
-//           const consoleLogStub = [];
-//           const originalConsoleLog = console.log;
-//           console.log = (message) => {
-//             consoleLogStub.push(message);
-//           };
-  
-//           Computer.welcome(comp1, comp2);
-  
-//           expect(consoleLogStub).to.deep.equal([
-//             "Dell PC powers on with Windows",
-//             "Apple Apple powers on with MacOS",
-//           ]);
-  
-//           console.log = originalConsoleLog;
-//         });
-//       });
-//     });
-  
